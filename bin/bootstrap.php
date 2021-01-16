45569822dd88ed46945328361243d22060ca1628
Vitaliy <theyamshikov@yandex.ru>
2021-01-16 20:38:51 +0600

    upd

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -10,17 +10,6 @@ use ZnLib\Console\Symfony4\Helpers\CommandHelper;
  * @var Container $container
  */
 
-//$container = Container::getInstance();
-
-// --- Application ---
-
-//$container->bind(Application::class, Application::class, true);
-
-// --- Generator ---
-
-//$container->bind(\ZnTool\Generator\Domain\Interfaces\Services\DomainServiceInterface::class, \ZnTool\Generator\Domain\Services\DomainService::class);
-//$container->bind(\ZnTool\Generator\Domain\Interfaces\Services\ModuleServiceInterface::class, \ZnTool\Generator\Domain\Services\ModuleService::class);
-
 CommandHelper::registerFromNamespaceList([
     'ZnTool\Phar\Commands',
 ], $container);
